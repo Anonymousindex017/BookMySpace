f1db4a026e78c7717cc41b31f1d240d69a01d990
Anonymousindex017 <ravi.kashyap181855@gmail.com>
2024-04-09 17:57:52 +0530

    feedbackuser

--- frontend/src/app/(main)/browse/page.jsx
+++ frontend/src/app/(main)/browse/page.jsx
@@ -44,20 +44,22 @@ const Browse = () => {
             />
           </svg>
         </button>
+        <div className='grid grid-cols-3 gap-10'>
+
         {
           spacelist.map((space) => {
             return (
-              <div>
-
-              <div className="w-full my-5 mx-auto max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+              <div className="my-5 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                 <a href="#">
                   <img className="rounded-t-lg" src="https://img.freepik.com/premium-photo/modern-office-space-with-desktops-with-modern-computers-created-with-generative-ai-technology_185193-110090.jpg" alt="" />
                 </a>
+                <div className='p-4 sm:p-8'>
+
                 <h5 className="mb-4 text-xl font-medium text-white dark:text-white">
                   Your Order
                 </h5>
                 <div className="flex items-baseline text-gray-900 dark:text-white">
-                
+
                   <span className="text-3xl font-semibold">Rs</span>
                   <span className="text-5xl font-extrabold tracking-tight">1000</span>
                   <span className="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400">
@@ -121,7 +123,7 @@ const Browse = () => {
                       Confrence Meeting Room
                     </span>
                   </li>
-                  
+
                   <li className="flex line-through decoration-gray-500">
                     <svg
                       className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500"
@@ -133,7 +135,7 @@ const Browse = () => {
                       <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
                     </svg>
                     <span className="text-base font-normal leading-tight text-gray-500 ms-3">
-                      
+
                       wifi Services
                     </span>
                   </li>
@@ -162,36 +164,37 @@ const Browse = () => {
                       <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
                     </svg>
                     <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
-                    24×7 phone &amp; email support
+                      24×7 phone &amp; email support
                     </span>
                   </li>
                 </ul>
-                  <Link
-                    href={"/space-details/"+space._id}
-                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                <Link
+                  href={"/space-details/" + space._id}
+                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Read more
+                  <svg
+                    className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
                   >
-                    Read more
-                    <svg
-                      className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 14 10"
-                    >
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M1 5h12m0 0L9 1m4 4L9 9"
-                      />
-                    </svg>
-                  </Link>
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </Link>
+                </div>
               </div>
-            </div>
             )
           })
         }
+        </div>
 
       </>
 
