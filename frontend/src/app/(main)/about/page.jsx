35f75996333a7ed8bf4b2a7b06721f86026859c5
Anonymousindex017 <ravi.kashyap181855@gmail.com>
2025-01-19 18:36:01 +0530

    new

--- frontend/src/app/(main)/about/page.jsx
+++ frontend/src/app/(main)/about/page.jsx
@@ -47,7 +47,7 @@ const About = () => {
                     className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-xl bg-white border border-transparent text-black hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none"
                     href="#"
                   >
-                    Read Case Studies
+                    Read Case Studies Successfull
                   </a>
                 </div>
               </div>
